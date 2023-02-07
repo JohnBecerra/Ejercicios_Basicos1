6491a9d00247f7dde6815ce1012f8589a9f28bce
John Sebastian Becerra Granados <jsbecerra@elcorral.loc>
2023-02-07 18:56:01 -0500

    segundo commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        System.out.println("Bienvenidos a la clase de CP3");
     }
+
 }
