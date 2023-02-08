24f5e4811b948660a03fb5e68cb3c4f0f092e4b4
jsbecerra <jhon.becerra@uniminuto.edu.co>
2023-02-07 19:01:49 -0500

    Ajuste de salida por consola

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
         System.out.println("Hello world!");
 
         System.out.println("Bienvenidos a la clase de CP3");
+
+        System.out.println("En esta clase van a aprender Git");
     }
 
 }
